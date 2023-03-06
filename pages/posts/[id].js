a95724c13d778649b59080f13e89f8a5c95702c8
Shazammon <ammonorgill@gmail.com>
2023-03-06 12:13:24 -0700

    updated date componennt in home file

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,7 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head';
+import Date from '../../components/date';
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -27,7 +28,7 @@ export default function Post({ postData }) {
         </Head>
         {postData.title}
         <br />
-        {postData.id}
+        <Date dateString={postData.id} />
         <br />
         {postData.date}
         <br />
